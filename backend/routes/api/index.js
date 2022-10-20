ad477ed46dbad9b7d44e5cf0538cfc88f9eca804
D0RK5TER <pdassaf@gmail.com>
2022-10-20 10:16:08 -0700

    login is done

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const sessionRouter = require('./session.js')
+const usersRouter = require('./users.js');
 const { restoreUser } = require('../../utils/auth.js');
 
 router.use(restoreUser);
@@ -12,6 +13,13 @@ router.get(
 );
 router.use(restoreUser);
 
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', (req, res) => {
+    res.json({ requestBody: req.body });
+});
 // router.get('/test', function (req, res) {
 //     res.json({ requestBody: req.body });
 // });
