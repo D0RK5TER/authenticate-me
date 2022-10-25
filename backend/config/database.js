791b8970baab42c17e7fda86b57510cd8a2c0c23
D0RK5TER <pdassaf@gmail.com>
2022-10-25 16:11:47 -0700

    jeez

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,8 +4,8 @@ const config = require('./index');
 module.exports = {
   development: {
     storage: config.dbFile,
-    dialect: 'sqlite',
-    seederStorage: 'sequelize',
+    dialect: "sqlite",
+    seederStorage: "sequelize",
     logQueryParameters: true,
     typeValidation: true
   },
